5779481bfdb124e4871ec7244fe8b5ee3627b093
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-17 01:56:11 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from pyrogram import Client
 from plugins.video_cut import start_command
 from plugins.video_to_audio import start_command
 
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
-BOT_TOKEN = 'your_bot_token'
+API_ID = '3335796'
+API_HASH = '138b992a0e672e8346d8439c3f42ea78'
+BOT_TOKEN = '5088657122:AAHdusGDuWfBpSDWkcX-qU1_fgzij4w8Lzk'
 
 logging.basicConfig(level=logging.INFO)
 
