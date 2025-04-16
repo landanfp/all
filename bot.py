f84b37cf39c4352165f89dd56d2fcc2f10fdddf9
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-16 18:21:05 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-"""
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
@@ -7,14 +7,14 @@ import os
 import shutil
 from pyrogram.types import InputMediaVideo
 from aiohttp import web
-import asyncio """
+import asyncio 
 #import ffmpeg
 
 
-import os
-import asyncio
-from pyrogram import Client
-from aiohttp import web
+#import os
+#import asyncio
+#from pyrogram import Client
+#from aiohttp import web
 
 API_ID = int(os.environ.get("API_ID", "3335796"))
 API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
