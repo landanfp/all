765e931b542f26e414d2d1f328e3ae1c5c22bfb2
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-16 17:23:18 +0330

    Create cut.py

---
+++ plugins/cut.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
+
+@Client.on_callback_query(filters.regex("^video_cut$"))
+async def handle_video_cut(client, callback_query: CallbackQuery):
+    await callback_query.answer()  # پاسخ سریع به کلیک
+    await callback_query.message.reply_text("به ابزار برش ویدیو خوش آمدید!")
