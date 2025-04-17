8abe1a9f74bc53f3903ab2e206d164a42102e838
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-17 17:05:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 API_ID = 3335796
 API_HASH = "138b992a0e672e8346d8439c3f42ea78"
-BOT_TOKEN = "5355055672:AAHoidc0x6nM3g2JHmb7xhWKmwGJOoKFNXY"
+BOT_TOKEN = "7136875110:AAFzyr2i2FbRrmst1sklkJPN7Yz2rXJvSew"
 
 plugins = dict(root="plugins")
 
