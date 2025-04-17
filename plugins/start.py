c42d4844583d7f21fe5a333a426a5fd27902d485
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-17 11:49:54 +0330

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,6 +1,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+import cutvideo
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message: Message):
