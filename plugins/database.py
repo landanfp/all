717c9395c0eefc05c2da8c5c719fd62236706a16
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-17 16:49:17 +0330

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,28 +1,20 @@
 import os
-import aiofiles
-import time
 from pyrogram.types import Message
 
 async def download_file_stream(client, file_id, user_id, message: Message):
     file_path = f"{user_id}_original.mp4"
 
     async def progress(current, total):
-        percent = current * 100 / total
-        done = int(percent / 5)
+        # نمایش حجم دانلود شده و کل حجم
         await message.edit_text(
-            f"در حال دانلود: {percent:.1f}%"
+            f"در حال دانلود...\n"
             f"حجم: {current / (1024*1024):.2f}MB / {total / (1024*1024):.2f}MB"
         )
 
     file = await client.download_media(file_id, file_name=file_path, progress=progress)
     return file
 
+# حذف بخش ارسال فایل به کانال لاگ
 async def upload_to_log_channel(client, file_path, user_id, message: Message):
-    async def progress(current, total):
-        percent = current * 100 / total
-        await message.edit_text(
-            f"در حال آپلود: {percent:.1f}%"
-            f"حجم: {current / (1024*1024):.2f}MB / {total / (1024*1024):.2f}MB"
-        )
-
-    await client.send_document("me", document=file_path, caption="برش ویدیو کاربر")
+    # این تابع حذف شد، چون نیازی به ارسال فایل به کانال لاگ نیست.
+    pass
