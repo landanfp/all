63a14f76c8d69a8289a1923ee5c5be05de06a92b
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-17 13:36:41 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,7 @@
-import os
+from pyrogram import Client
+from fastapi import FastAPI
 import threading
 import uvicorn
-from fastapi import FastAPI
-from pyrogram import Client
-
-# مسیر مطلق برای پوشه plugins
-plugin_path = os.path.join(os.path.dirname(__file__), "plugins")
 
 # Pyrogram bot setup
 app = Client(
@@ -13,7 +9,7 @@ app = Client(
     api_id=3335796,
     api_hash="138b992a0e672e8346d8439c3f42ea78",
     bot_token="6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8",
-    plugins=dict(root=plugin_path)
+    plugins=dict(root="plugins")  # فقط همین مسیر نسبی رو بذار
 )
 
 # FastAPI app for health check
@@ -28,6 +24,5 @@ def run_web():
 
 if __name__ == "__main__":
     print("Bot is running...")
-    print("Plugin path:", plugin_path)
     threading.Thread(target=run_web).start()
     app.run()
