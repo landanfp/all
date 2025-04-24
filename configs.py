44b0db04d41b86d53f7518a8d431a46922414b44
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-24 20:36:01 +0330

    Delete configs.py

--- configs.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5088657122:AAGXARfg6sSX1p1ge876jknkrJizwH959b4")
-    API_ID = int(os.environ.get("API_ID", "3335796"))
-    API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
