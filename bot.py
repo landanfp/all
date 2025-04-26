bd632a0b51d740ae3cddc75d1b4a89f119e9ff7b
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-26 16:09:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8'
+BOT_TOKEN = '1396293494:AAE6YAY-Vog3QPvSNCo8x80FsIue9FJGWh8'
 LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("watermark_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
