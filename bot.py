64553636ea7a48c4f4135122cd3eb490a46d49cb
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-26 16:57:12 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import tempfile
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '7136875110:AAFzyr2i2FbRrmst1sklkJPN7Yz2rXJvSew'
+BOT_TOKEN = '6975247999:AAEaK2CYU4FpgZ8ruW8ZxzXfGQ9dsXuepuw'
 LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("watermark_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
