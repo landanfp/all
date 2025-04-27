47c75ff0e096b3ee181f6cd8f7de48e232fc099d
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 12:29:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ API_HASH = '138b992a0e672e8346d8439c3f42ea78'
 BOT_TOKEN = '6975247999:AAEaK2CYU4FpgZ8ruW8ZxzXfGQ9dsXuepuw'
 LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
-
-app = Client(
-    "hardsub_bot",
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH"),
-    bot_token=os.environ.get("BOT_TOKEN")
-)
+app = Client("hardsub_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 app.run()
