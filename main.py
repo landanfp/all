0d33c0972e5cdfe8f580bffec12e5d614364f7fe
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 12:24:52 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+from plugins import start, hardsub, upload
+
+app = Client(
+    "hardsub_bot",
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH"),
+    bot_token=os.environ.get("BOT_TOKEN")
+)
+
+app.run()
