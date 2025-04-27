eedddf3cf36df1102e1b1939d3e85cd7cf5c95eb
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 13:16:08 +0330

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
 from helper.progress import progress_bar
+from loader import app
 
-@Client.on_message(filters.document)
+@app.on_message(filters.document)
 async def catch_subtitle(client, message):
     # فرض بر اینه که فایل srt باشه و تو مرحله بعد hardsub بشه
     await message.download(file_name="subtitle.srt", progress=progress_bar, progress_args=("دانلود زیرنویس", message))
