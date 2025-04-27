eb95445ad2e5a4aa1169450f5455d3897eabca57
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 13:04:31 +0330

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
 
-@app.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("سلام! ویدیوت رو بفرست و زیرنویس .srt رو هم بفرست تا بچسبونم.")
