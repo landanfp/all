0c654f26f4528dbf0c54837cefe29784a253b35d
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 12:26:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from pyrogram import Client
 from plugins import start, hardsub, upload
 
+API_ID = '3335796'
+API_HASH = '138b992a0e672e8346d8439c3f42ea78'
+BOT_TOKEN = '6975247999:AAEaK2CYU4FpgZ8ruW8ZxzXfGQ9dsXuepuw'
+LOG_CHANNEL = -1001792962793  # مقدار دلخواه
+
+
 app = Client(
     "hardsub_bot",
     api_id=int(os.environ.get("API_ID")),
