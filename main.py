8fb1586f030242d552ddebf4e30de5fd02539d2f
landanfp <159138257+landanfp@users.noreply.github.com>
2025-04-27 18:06:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import threading
+from fastapi import FastAPI
+import uvicorn
+
 from loader import app
 from plugins import start, hardsub, upload
 
+# FastAPI app برای پاسخ دادن به Health Check
+api = FastAPI()
+
+@api.get("/")
+async def root():
+    return {"message": "Bot is running."}
+
+# اجرای سرور در یک ترد جدا
+def run_server():
+    uvicorn.run(api, host="0.0.0.0", port=8000)
+
+# شروع سرور
+threading.Thread(target=run_server).start()
+
+# اجرای اپلیکیشن پایروگرام
 app.run()
