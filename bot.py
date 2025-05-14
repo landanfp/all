45663600814b3801d25776023be02b92800eb0c2
landanfp <159138257+landanfp@users.noreply.github.com>
2025-05-14 17:35:14 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import time
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+API_ID = '3335796'
+API_HASH = '138b992a0e672e8346d8439c3f42ea78'
+BOT_TOKEN = '6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8'
 
 
 bot = Client("compressor_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
