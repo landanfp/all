f2aa4d09320cfabbcb880da81d8d0ba569fc2978
landanfp <159138257+landanfp@users.noreply.github.com>
2025-05-14 15:16:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ from threading import Thread
 import json
 
 # اطلاعات ربات (لطفاً با مقادیر واقعی خود جایگزین کنید)
-API_ID = os.environ.get('API_ID', 'YOUR_API_ID') # <--- مقدار واقعی خود را اینجا قرار دهید
-API_HASH = os.environ.get('API_HASH', 'YOUR_API_HASH') # <--- مقدار واقعی خود را اینجا قرار دهید
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN') # <--- مقدار واقعی خود را اینجا قرار دهید
+API_ID = '3335796'
+API_HASH = '138b992a0e672e8346d8439c3f42ea78'
+BOT_TOKEN = '6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8'
 
 app = Client("watermark_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
