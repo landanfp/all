1f7e539dd27908c682e4e9c9ecf9f8c31a1bed90
landanfp <159138257+landanfp@users.noreply.github.com>
2025-05-15 14:41:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, idle, filter  # filters را به filter تغییر دادیم
+from pyrogram import Client, idle
+from pyrogram import filters  # ایمپورت صحیح filters
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from plugins.image_watermark import (
     ask_image,
@@ -23,17 +24,17 @@ API_HASH = '138b992a0e672e8346d8439c3f42ea78'
 app = Client("watermark_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
 # اضافه کردن هندلرها
-app.add_handler(MessageHandler(start_handler, filter.command("start")))
-app.add_handler(CallbackQueryHandler(ask_text, filter.regex("text_wm")))
-app.add_handler(MessageHandler(handle_text_input, filter.text & filter.private))
-app.add_handler(CallbackQueryHandler(set_position, filter.regex("^text_pos_")))
-app.add_handler(CallbackQueryHandler(set_size, filter.regex("^text_size_")))
-app.add_handler(MessageHandler(handle_video, filter.video & filter.private))
-app.add_handler(CallbackQueryHandler(ask_image, filter.regex("image_wm")))
-app.add_handler(MessageHandler(handle_image_upload, filter.photo & filter.private))
-app.add_handler(CallbackQueryHandler(set_image_position, filter.regex("^image_pos_")))
-app.add_handler(CallbackQueryHandler(set_image_size, filter.regex("^image_size_")))
-app.add_handler(MessageHandler(process_image_watermark, filter.video & filter.private))
+app.add_handler(MessageHandler(start_handler, filters.command("start")))
+app.add_handler(CallbackQueryHandler(ask_text, filters.regex("text_wm")))
+app.add_handler(MessageHandler(handle_text_input, filters.text & filters.private))
+app.add_handler(CallbackQueryHandler(set_position, filters.regex("^text_pos_")))
+app.add_handler(CallbackQueryHandler(set_size, filters.regex("^text_size_")))
+app.add_handler(MessageHandler(handle_video, filters.video & filters.private))
+app.add_handler(CallbackQueryHandler(ask_image, filters.regex("image_wm")))
+app.add_handler(MessageHandler(handle_image_upload, filters.photo & filters.private))
+app.add_handler(CallbackQueryHandler(set_image_position, filters.regex("^image_pos_")))
+app.add_handler(CallbackQueryHandler(set_image_size, filters.regex("^image_size_")))
+app.add_handler(MessageHandler(process_image_watermark, filters.video & filters.private))
 
 
 app.run()
