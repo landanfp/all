1917bf0677f2f81b2af09443aa8893243b173cdf
landanfp <159138257+landanfp@users.noreply.github.com>
2025-05-15 14:34:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,15 @@
-from pyrogram import Client
-#from config import BOT_TOKEN
-#from pyrogram import Client
-from pyrogram import idle
+from pyrogram import Client, idle
 from pyrogram.handlers import MessageHandler
-#from plugins import image_watermark, text_watermark, start   # این مهمه
+from pyrogram.plugins import load_plugins  # ایمپورت کردن load_plugins
+from plugins import image_watermark, text_watermark, start
 
 BOT_TOKEN = '6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8'
-API_ID = '3335796' 
+API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-
 
 app = Client("watermark_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
 # لود کردن پلاگین‌ها
 load_plugins("plugins")
-
 
 app.run()
