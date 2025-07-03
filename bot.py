cbaa467a7c1e6421d4c6b2f2d577af462407ae3b
landanfp <159138257+landanfp@users.noreply.github.com>
2025-07-03 14:00:24 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import asyncio
 # Bot configuration
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '5355055672:AAHoidc0x6nM3g2JHmb7xhWKmwGJOoKFNXY'
+BOT_TOKEN = '7136875110:AAGr1EREy_qPMgxVbuE4B0cHGVcwWudOrus'
 #LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("watermark_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
