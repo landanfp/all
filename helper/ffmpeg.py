3d635bc7c7a7c1b17936889107dc6c6451fe90d9
landanfp <159138257+landanfp@users.noreply.github.com>
2025-09-18 13:41:59 +0330

    Delete helper directory

--- helper/ffmpeg.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-async def add_hardsub(input_video, input_subtitle, output_video):
-    # Using ultrafast preset and 1 thread to minimize RAM usage
-    cmd = f'ffmpeg -y -i "{input_video}" -vf subtitles="{input_subtitle}":force_style="Fontsize=24,PrimaryColour=&HFFFFFF&" -preset ultrafast -threads 1 -f mp4 "{output_video}"'
-    proc = await asyncio.create_subprocess_shell(cmd)
-    await proc.communicate()
