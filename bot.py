15cc2b4ff512b20b36cb3312a2b1d6067f675866
landanfp <159138257+landanfp@users.noreply.github.com>
2025-10-24 20:49:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '5355055672:AAHoidc0x6nM3g2JHmb7xhWKmwGJOoKFNXY'
+BOT_TOKEN = '5355055672:AAEE8OIOqLYxbnwesF3ki2sOsXr03Q90JiI'
 LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("trim_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
