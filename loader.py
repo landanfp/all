4cffeea1ac4af7640326982c80aec63d944184ea
landanfp <159138257+landanfp@users.noreply.github.com>
2025-10-25 15:01:23 +0330

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ import os
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '7136875110:AAGr1EREy_qPMgxVbuE4B0cHGVcwWudOrus'
+BOT_TOKEN = '5355055672:AAEE8OIOqLYxbnwesF3ki2sOsXr03Q90JiI'
 
 plugins = dict(root="plugins")
 
