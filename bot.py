f24b61065490bfc1cf821c7da1775dbaf8870607
landanfp <159138257+landanfp@users.noreply.github.com>
2025-10-25 14:44:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import json
 # اطلاعات ربات (لطفاً با مقادیر واقعی خود جایگزین کنید)
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '6964975788:AAH3OrL9aXHuoIUliY6TJbKqTeR__X5p4H8'
+BOT_TOKEN = '5355055672:AAEE8OIOqLYxbnwesF3ki2sOsXr03Q90JiI'
 
 app = Client("watermark_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
