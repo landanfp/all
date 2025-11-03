32d58bde32d023b8276cb28c03ff88c3cc111f40
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-03 20:14:41 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import threading
 # --- تنظیمات (Configuration) ---
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '1396293494:AAFY7RXygNEZPFPXfmoJ66SljlXeCSilXG0'
+BOT_TOKEN = '8189638115:AAEYMDvummCXAPgdpavZbYHa3YuXpOzkRBY'
 #LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("trim_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
