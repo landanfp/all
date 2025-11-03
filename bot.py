dcccadb139d488c8e2e9b8196271ad95e5271faf
landanfp <159138257+landanfp@users.noreply.github.com>
2025-11-03 21:53:21 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,18 +15,6 @@ from audio_trimmer import handle_audio_file, cut_audio_action
 
 # --- تنظیمات (Configuration) ---
 
-API_ID = '3335796'
-API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '8189638115:AAEYMDvummCXAPgdpavZbYHa3YuXpOzkRBY'
-#LOG_CHANNEL = -1001792962793  # مقدار دلخواه
-
-app = Client("trim_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-user_state = {}
-
-def seconds_to_hms(seconds):
-    """تبدیل ثانیه به فرمت زمان (hh:mm:ss)."""
-    return str(timedelta(seconds=seconds))
 
 # ===============================================
 # --- بخش Health Check ---
